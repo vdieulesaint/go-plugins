3a53f62f456d0f4907aba3b69a2cabd8650e0d9a
Asim Aslam <asim@aslam.me>
2019-07-30 12:38:54 +0100

    go fmt

--- transport/nats/nats_test.go
+++ transport/nats/nats_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 
 	"github.com/go-log/log"
-	"github.com/micro/go-micro/transport"
 	"github.com/micro/go-micro/server"
+	"github.com/micro/go-micro/transport"
 	"github.com/nats-io/nats.go"
 )
 
